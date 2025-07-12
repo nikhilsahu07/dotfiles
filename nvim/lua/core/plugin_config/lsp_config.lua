5317f409f6eec600a20ec4d41d3bb7ea5a8b9e5a
Nikhil Sahu <heynikhil07@gmail.com>
2025-07-12 16:38:06 +0530

    remove go lsp as it is too heavy and resource intensive

--- nvim/lua/core/plugin_config/lsp_config.lua
+++ nvim/lua/core/plugin_config/lsp_config.lua
@@ -1,5 +1,5 @@
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls", "ts_ls", "gopls", "yamlls", "dockerls", "jsonls", "bashls" }
+  ensure_installed = { "ts_ls", "yamlls", "dockerls", "jsonls", "bashls" }
 })
 
 local lspconfig = require('lspconfig')
@@ -83,8 +83,6 @@ require("lspconfig").lua_ls.setup {
 }
 
 require("lspconfig").ts_ls.setup({})
-require("lspconfig").gopls.setup({})
-require("lspconfig").tailwindcss.setup({})
 require("lspconfig").yamlls.setup({})
 require("lspconfig").dockerls.setup({})
 require("lspconfig").jsonls.setup({})
